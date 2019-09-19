593303093f9d63477b9326fc1ea149189d5c61cd
AJRenion <54093669+AJRenion@users.noreply.github.com>
2019-09-19 13:29:44 +0800

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,17 @@
+    #include<stdio.h>
+     
+    long addition(long, long);
+     
+    main()
+    {
+       long first, second, sum;
+     
+       scanf("%ld%ld", &first, &second);
+     
+       sum = addition(first, second);
+     
+       printf("%ld\n", sum);
+     
+       return 0;
+    }
+     
